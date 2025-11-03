6004ed4ef46a94b2e570b71eb02211dc6580667e
Backend12 <backend12@Erxess-Mac-mini.local>
2025-11-03 20:25:02 +0800

    Over

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import { print } from "./print.js";
-print();
+import * as fs from "node:fs";
+fs.writeFile("example.txt","Hello","utf-8",() => {
+    fs.readFile("example.txt","utf-8",(error,data) => {
+        console.log(data);
+    })
+})
