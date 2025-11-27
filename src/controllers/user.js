8fa1fdcb9149322417aaf7e8f67599d96903ced9
Backend12 <backend12@Erxess-Mac-mini.local>
2025-11-27 17:15:03 +0800

    .

---
+++ src/controllers/user.js
@@ -0,0 +1,17 @@
+export const login = (req, res) => {
+
+    
+  res.cookie("user", "userId123", {
+    httpOnly: true,
+    secure: false,
+  });
+  res.json({
+    user: "userId123",
+  });
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("user");
+
+  res.send("Success!");
+};
