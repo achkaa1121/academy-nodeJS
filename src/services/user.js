5fe3a83dd65a132c660023653e45ed7cbbb16728
Backend12 <backend12@Erxess-Mac-mini.local>
2025-12-02 20:12:00 +0800

    update

---
+++ src/services/user.js
@@ -0,0 +1,59 @@
+import { db } from "../db.js";
+
+export const createUserService = async (username, email, password) => {
+  const response = await db.query(
+    `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`,
+    [username, email, password]
+  );
+  return response.rows[0];
+};
+
+export const getUsersService = async () => {
+  const response = await db.query("SELECT * FROM users");
+  return response.rows;
+};
+
+export const updateUserService = async (
+  id,
+  username,
+  email,
+  password,
+  firstname,
+  lastname
+) => {
+  const { rows } = await db.query(
+    `UPDATE users 
+     SET username = $1, email = $2, password = $3, firstname = $4, lastname = $5 
+     WHERE id = $6 
+     RETURNING *`,
+    [username, email, password, firstname, lastname, id]
+  );
+
+  return rows[0];
+};
+
+export const getUserByIdService = async (id) => {
+  const response = await db.query(`SELECT * FROM users WHERE id = ${id}`);
+  return response.rows[0];
+};
+
+export const deleteUserService = async (id) => {
+  const response = await db.query(
+    `DELETE FROM users WHERE id = ${id} RETURNING *`
+  );
+  return response.rows[0];
+};
+
+export const getUserAccountsService = async (id) => {
+  const response = await db.query(
+    `SELECT * FROM accounts WHERE user_id = ${id}`
+  );
+  return response.rows;
+};
+
+export const getUserTransactionsService = async (id) => {
+  const response = await db.query(
+    `SELECT * FROM transactions WHERE user_id = ${id}`
+  );
+  return response.rows;
+};
