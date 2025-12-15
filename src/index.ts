87d37bd2ccfee789670ce09e195be778f81da304
Backend12 <backend12@Erxess-Mac-mini.local>
2025-12-15 20:22:02 +0800

    typescript integrate hiiv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import type { Request, Response } from "express";
 import { userRouters } from "./routers/user.js";
 import { bankRouters } from "./routers/bank.js";
 import { connectDb } from "./db.js";
+import { request } from "express";
 
 const app = express();
 
